b0cf995523875068c100d268b9f0d3aeb11f8b56
Yogesh <Yogesh@LAPTOP-IRFJLKTO>
2021-02-12 11:42:07 +0530

    New Commit

---
+++ FebFeetApp/src/main/java/com/coforge/FebfeetApp/App.java
@@ -0,0 +1,13 @@
+package com.coforge.FebfeetApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
